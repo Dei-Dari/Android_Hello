6d999045a82a89bbcfe93f5606c41f7533ea31b0
Дарья ... <batterfly74@yandex.ru>
2024-07-02 00:14:12 +0500

    Initial commit

---
+++ app/src/main/java/com/myapplication/MainActivity.java
@@ -0,0 +1,99 @@
+package com.myapplication;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+
+    //TextView textView;
+
+    Button toastButton;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        Toast.makeText(MainActivity.this, "активность создается", Toast.LENGTH_LONG).show();
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        //textView=findViewById(R.id.textview);
+
+        toastButton = findViewById(R.id.toast);
+
+        toastButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this, "кнопка нажата", Toast.LENGTH_LONG).show();
+                //Log.d("MainActivity", "Мы нажали кнопку");
+                // явный вызов
+                Intent intent = new Intent(MainActivity.this, SecondActivity.class);
+                //startActivity(intent);
+                //неявный вызов
+//                Uri adress = Uri.parse("www.google.com");
+//                Intent intent = new Intent(Intent.ACTION_VIEW, adress);
+//                // если нет предустановленного браузера
+//                if (intent.resolveActivity(getPackageManager()) != null) {
+//                    startActivity(intent);
+//                } else {
+//                    //Log.d("er", "Ошибка");
+//                    Toast.makeText(MainActivity.this,"ошибка", Toast.LENGTH_SHORT).show();
+//
+//                }
+
+            }
+        });
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Toast.makeText(MainActivity.this, "активность становится видимой для пользователя", Toast.LENGTH_LONG).show();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Toast.makeText(MainActivity.this, "активность выходит на передний план", Toast.LENGTH_LONG).show();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Toast.makeText(MainActivity.this, "активность уходит с переднего плана", Toast.LENGTH_LONG).show();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Toast.makeText(MainActivity.this, "активность перестает быть видимой", Toast.LENGTH_LONG).show();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Toast.makeText(MainActivity.this, "активность уничтожается", Toast.LENGTH_LONG).show();
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        Toast.makeText(MainActivity.this, "пользователь переходит к активности", Toast.LENGTH_LONG).show();
+    }
+}
